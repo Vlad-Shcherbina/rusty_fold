a88c9a7a102f96d9a52e2dcd114051f52e65a20e
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-07-03 20:46:44 +0300

    Segment intersection

--- src/geom.rs
+++ src/geom.rs
@@ -1,6 +1,7 @@
-use num_traits::FromPrimitive;
+use num_traits::{FromPrimitive, Zero};
 use crate::prelude::*;
 
+#[derive(PartialEq, Eq)]
 pub struct Vec2 {
     pub x: BigRational,
     pub y: BigRational,
@@ -20,6 +21,32 @@ impl Vec2 {
             y: y.parse().unwrap(),
         }
     }
+
+    fn cross(&self, other: &Vec2) -> BigRational {
+        &self.x * &other.y - &self.y * &other.x
+    }
+}
+
+impl std::ops::Sub<&Vec2> for &Vec2 {
+    type Output = Vec2;
+
+    fn sub(self, rhs: &Vec2) -> Self::Output {
+        Vec2 {
+            x: &self.x - &rhs.x,
+            y: &self.y - &rhs.y,
+        }
+    }
+}
+
+impl std::ops::Mul<&BigRational> for &Vec2 {
+    type Output = Vec2;
+
+    fn mul(self, rhs: &BigRational) -> Self::Output {
+        Vec2 {
+            x: &self.x * rhs,
+            y: &self.y * rhs,
+        }
+    }
 }
 
 fn iter_edges(poly: &[Vec2]) -> impl Iterator<Item=(&Vec2, &Vec2)> {
@@ -34,4 +61,56 @@ pub fn area(poly: &[Vec2]) -> BigRational {
         s += (&a.x + &b.x) * (&b.y - &a.y);
     }
     s / BigRational::from_i32(2).unwrap()
+}
+
+pub fn segment_intersection(seg1: (&Vec2, &Vec2), seg2: (&Vec2, &Vec2)) -> Option<Vec2> {
+    let bb_x1 = (&seg1.0.x).min(&seg1.1.x).max((&seg2.0.x).min(&seg2.1.x));
+    let bb_y1 = (&seg1.0.y).min(&seg1.1.y).max((&seg2.0.y).min(&seg2.1.y));
+    let bb_x2 = (&seg1.0.x).max(&seg1.1.x).min((&seg2.0.x).max(&seg2.1.x));
+    let bb_y2 = (&seg1.0.y).max(&seg1.1.y).min((&seg2.0.y).max(&seg2.1.y));
+
+    let d1 = seg1.1 - seg1.0;
+    let d2 = seg2.1 - seg2.0;
+
+    let c = d1.cross(&d2);
+    if c.is_zero() {
+        cov_mark::hit!(collinear);
+        return None;
+    }
+    let alpha = d1.cross(&(seg2.0 - seg1.0));
+    let  beta = d1.cross(&(seg2.1 - seg1.0));
+    assert_ne!(alpha, beta);
+
+    let pt = &(&(seg2.0 * &beta) - &(seg2.1 * &alpha)) * &(beta - alpha).recip();
+    if *bb_x1 <= pt.x && pt.x <= *bb_x2 &&
+       *bb_y1 <= pt.y && pt.y <= *bb_y2 {
+        Some(pt)
+    } else {
+        cov_mark::hit!(intersection_outside);
+        None
+    }
+}
+
+#[test]
+fn test_segment_interseciton() {
+    let res = segment_intersection(
+        (&Vec2::parse("1,0"), &Vec2::parse("2,2")),
+        (&Vec2::parse("2,0"), &Vec2::parse("1,1")));
+    assert_eq!(res, Some(Vec2::parse("4/3,2/3")));
+
+    {
+        cov_mark::check!(collinear);
+        let res = segment_intersection(
+            (&Vec2::parse("1,2"), &Vec2::parse("2,4")),
+            (&Vec2::parse("0,0"), &Vec2::parse("3,6")));
+        assert_eq!(res, None);
+    }
+
+    {
+        cov_mark::check!(intersection_outside);
+        let res = segment_intersection(
+            (&Vec2::parse("0,0"), &Vec2::parse("1,1")),
+            (&Vec2::parse("2,1"), &Vec2::parse("3,0")));
+        assert_eq!(res, None);
+    }
 }
